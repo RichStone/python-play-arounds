389f81a14cf2bd1cbb0eb55c3e9765ad7407dd72
Rich Stone <richard.had@hotmail.de>
2017-05-16 15:27:45 +0200

    add blog class and connect with Post

--- src/app.py
+++ src/app.py
@@ -1,3 +1,16 @@
-from models.Post import Post
+from database import Database
+from models.Blog import Blog
 
- 
+Database.initialize()
+
+blog = Blog(author="Jose",
+            title="Sample Title",
+            description="Sample Description")
+
+blog.new_post()
+
+blog.save_to_mongo()
+
+from_database = Blog.from_mongo(blog.id)
+
+print(blog.get_posts())
